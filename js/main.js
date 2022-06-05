b4364fbe0e192731c72937fcb2c926eb3f593095
chenaoh <cdhenao@gmail,com>
2022-06-05 01:32:28 -0500

    se agrego jv

---
+++ js/main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+	const elementosCarousel = document.querySelectorAll('.carousel');
+	M.Carousel.init(elementosCarousel, {
+		duration: 150,
+		dist: -80,
+		shift: 3,
+		padding: 5,
+		numVisible: 5
+	});
+}); 
